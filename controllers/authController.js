ea605453066e6726326d75bc4397196704556669
ArshBiG <gameepass007@gmail.com>
2025-03-16 17:19:40 +0330

    iransay

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,21 +5,17 @@ const registerUser = async (req, res) => {
   try {
     const { name, familyName, phone, password } = req.body;
 
-    // چک کردن مقداردهی صحیح
     if (!name || !familyName || !phone || !password) {
       return res.status(400).json({ message: "همه فیلدها الزامی هستند." });
     }
 
-    // چک کردن کاربر تکراری
     const existingUser = await User.findOne({ phone });
     if (existingUser) {
       return res.status(400).json({ message: "این شماره موبایل قبلاً ثبت شده است." });
     }
 
-    // رمزگذاری پسورد
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // ذخیره در دیتابیس
     const newUser = new User({ name, familyName, phone, password: hashedPassword });
     await newUser.save();
 
